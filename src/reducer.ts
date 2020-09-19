5961e6c47ed156660431ca25572e95d61a5cf0d2
machamp0714 <rateliasudaisuki@gmail.com>
2020-09-19 15:02:52 +0900

    github reducerを作成

---
+++ src/reducer.ts
@@ -0,0 +1,45 @@
+import { Reducer } from 'redux';
+import { AxiosError } from 'axios';
+
+import { GithubAction } from './actions/github';
+import * as ActionType from './actions/githubConstants';
+import { User } from './services/github/models';
+
+export interface GithubState {
+  users: User[];
+  isLoading: boolean;
+  error?: AxiosError | null;
+}
+
+export const initState: GithubState = {
+  users: [],
+  isLoading: false
+};
+
+const githubReducer: Reducer<GithubState, GithubAction> = (
+  state: GithubState = initState,
+  action: GithubAction
+): GithubState => {
+  switch (action.type) {
+    case ActionType.GET_MEMBERS_START:
+      return {
+        ...state,
+        users: [],
+        isLoading: true,
+      }
+    case ActionType.GET_MEMBERS_SUCCESS:
+      return {
+        ...state,
+        users: action.payload.result.users,
+        isLoading: false
+      }
+    case ActionType.GET_MEMBERS_FAIL:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload.error
+      }
+  }
+}
+
+export default githubReducer;
